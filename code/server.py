39d66fdce8b99ecd7dea990f80dc8c557b0ce878
Geoff Howland <geoff@gmail.com>
2016-01-13 09:50:30 -0800

    Updated

--- code/server.py
+++ code/server.py
@@ -3,26 +3,53 @@ Simple example Flask server
 """
 
 import sys
-import flask
 
-SERVER = flask.Flask(__name__)
+from flask import Flask
+from flask import request
+
+# Create the Flask server.  It is module level as we are going to use decorators against it for our module functions.
+SERVER = Flask(__name__)
 
 #SERVER_ADDRESS = '0.0.0.0'	# All interfaces all connections
 SERVER_ADDRESS = '127.0.0.1'	# Connections only on localhost (safer)
 
+# Are we debugging the web server?  Get tracebacks, has a console for testing expressions
+DEBUG = True
 
-@SERVER.route('/')
-def hello_world():
-    return 'Hello World!'
+
+@SERVER.route('/', methods=['GET', 'POST'])
+def RenderIndex():
+  """Render the index, which cant provide a path variable."""
+  return RenderPage('')
+
+
+@SERVER.route('/<path:path>', methods=['GET', 'POST'])
+def RenderPath(path):
+  """Render any page that isn't an index, which provides us with a path.  
+  
+  Path allows slashes, but doesnt verify that any files exist or anything.
+  """
+  return RenderPage(path)
+
+
+def RenderPage(path):
+  # Set the index path, if we got nothing
+  if not path:
+    path = 'index.html'
+
+  output = 'Path: %s' % path
+
+  return output
 
 
 def Main(args=None):
+  """Create the server.  Start it up."""
   global SERVER
 
   if not args:
     args = []
   
-  SERVER.run(SERVER_ADDRESS)
+  SERVER.run(SERVER_ADDRESS, debug=DEBUG)
 
 
 if __name__ == '__main__':
